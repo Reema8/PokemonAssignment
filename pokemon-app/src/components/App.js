6a476139456e77da84c19925a652b80d77ee0923
Reema8 <reema.bhandari8@gmail.com>
2021-10-20 19:53:20 +0530

    Basic front end design

--- pokemon-app/src/components/App.js
+++ pokemon-app/src/components/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import './styles/App.css';
 
 class App extends Component {
+  consturctor() {
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="App">
-        hellohello
+        
       </div>
     );
   }
